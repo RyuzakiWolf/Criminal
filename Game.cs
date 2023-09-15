d8daf407e18a6bbbcab9764bcf0602ecaec0515e
Feverdream Wolf <wolxv03@protonmail.com>
2023-09-15 16:46:44 -0400

    Made title menu.

---
+++ Game.cs
@@ -0,0 +1,23 @@
+﻿namespace Criminal
+{
+    internal class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criminal: Interactive Antisocial Stories Generator\n");
+            Console.WriteLine("1. New");
+            Console.WriteLine("2. Continue");
+            Console.WriteLine("0. Exit");
+            switch (Console.ReadLine())
+            {
+                case "0":
+                    Environment.Exit(0);
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    break;    
+            }
+        }
+    }
+}
